b0d3b29ceaf574187218efa32ff6b9b9755f2fc7
Ryan Rutan <ryan.rutan@jivesoftware.com>
2015-11-04 14:11:12 -0600

    Simple script to help build a sample add-on

--- examples/jive_addon/build.sh
+++ examples/jive_addon/build.sh
@@ -7,7 +7,11 @@ UUID=`uuidgen`
 if [ -z "$1" ]
 then
 	echo "--------------------------------------------------------------------------"
-	echo "Usage:  build.sh https://your_service_url:port"
+	echo "Usage:  build.sh https://your_service_url:port [options..]"
+	echo
+	echo "Options:"
+	echo "	-no_config - Remove the Add-On Configuration Parameter"
+	echo "	-no_register - Remove the Registration Configuration Parameters"
 	echo
 	echo "Note:  This simply replaces the service_url value in ./meta.json"
 	echo "       You are also welcome to update the files as needed and zip manually"
@@ -16,6 +20,22 @@ else
 	echo
 	echo "Generating Add-On (./extension.zip) for $1 ..."
 	echo
+	while test $# -gt 0
+	do
+	    case "$1" in
+	        -no_config) 
+				echo Removing Add-On Configuration Registration ...
+				less meta.json | grep -v "config_url" > meta.json
+				echo
+	            ;;
+	        -no_register) 
+				echo Removing Registration Configuration Registration ...
+				less meta.json | grep -v "register_url" > meta.json
+				echo
+	            ;;
+	    esac
+	    shift
+	done
 	sed -i.bak "s/{{GUID_ADDON}}/${UUID}/g" meta.json.template && rm meta.json.template.bak
 	cat meta.json.template | sed -e "s|{{SERVICE_URL}}|"${1}"|g" > meta.json
 	zip -r extension.zip * -x build.sh -x .DS_Store -x meta.json.template -x extension.zip
