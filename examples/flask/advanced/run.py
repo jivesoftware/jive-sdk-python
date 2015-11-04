d1cf6cae2736523feec388083c4519577379a5df
Ryan Rutan <ryan.rutan@jivesoftware.com>
2015-11-04 11:30:17 -0600

    Added Logger statements to runtime.

--- examples/flask/advanced/run.py
+++ examples/flask/advanced/run.py
@@ -1,4 +1,14 @@
 # Run a test server.
 from app import app
+import logging
+import sys
+
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
+rootLogger.setLevel(logging.DEBUG)
 
 app.run(host='0.0.0.0', port=app.config['PORT'], debug=app.config['DEBUG'])
